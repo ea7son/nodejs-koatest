1efcc1b8860850586c7a4ffe59bd9a9ac109b9ee
zhangruofan <ea7son@gmail.com>
2015-12-09 10:25:11 +0800

    add users

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,9 @@ var router = require('koa-router')();
 router.get('/', function *(next) {
   this.body = 'this a users response!';
 });
-
+router.get('/info',function *(next){
+  yield this.render('info',{
+    username:"lily"
+  })
+});
 module.exports = router;
